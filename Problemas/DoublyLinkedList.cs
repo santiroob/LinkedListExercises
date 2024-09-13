62a807a3aaef9a4627b2e19afc0b16963bc1ea8f
santiroob <srobles@estudiantec.cr>
2024-09-12 21:23:36 -0600

    solucion gitignore

---
+++ Problemas/DoublyLinkedList.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problemas
+{
+    public class DoublyLinkedList
+    {
+        public Node Head {  get; set; }
+
+        public Node Last { get; set; }
+
+        public Node Middle { get; set; }
+        public int Count { get; set; }
+
+        public DoublyLinkedList() 
+        {
+            Count = 0;
+        }
+
+
+        public bool isEmpty()
+        {
+            return Head == null;
+        }
+
+        public void InsertLast(int value)
+        {
+            Node NewNode = new Node(value);
+
+            if (isEmpty())
+            {
+                Head = NewNode;
+                Last = Head;
+
+            }
+
+            else
+            {
+                NewNode.Previous = Last;
+                Last.Next = NewNode;
+                Last = NewNode;
+                
+            }
+
+            Count++;
+            UpdateMiddle();
+        }
+
+
+        public void UpdateMiddle()
+        {
+            if (Count == 0)
+            {
+                Middle = null;
+                return;
+            }
+
+            Node current = Head;
+            int middleIndex = Count / 2;
+
+            for (int i = 0; i < middleIndex; i++)
+            {
+                current = current.Next;
+            }
+
+            // Si el número de nodos es par, el nodo en `middleIndex` será el primero del par central
+            if (Count % 2 == 0)
+            {
+                Middle = current;
+            }
+            else
+            {
+                // Para una lista impar, `current` ya estará en el nodo central
+                Middle = current;
+            }
+        }
+
+
+
+
+        public void DeleteFirst()
+        {
+            Head = Head.Next;
+            Head.Previous = null;
+            Count--;
+            UpdateMiddle();
+        }
+
+        public void DeleteLast()
+        {
+            Last = Last.Previous;
+            Last.Next = null;
+            Count--;
+            UpdateMiddle();
+        }
+
+        public void PrintList()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("The list is empty.");
+                return;
+            }
+
+            Node current = Head;
+            Console.Write("LA LISTA ES: ");
+
+            while (current != null)
+            {
+                Console.Write(current.Data + " ");
+                current = current.Next;
+            }
+
+            Console.WriteLine(); // Salto de línea al final
+        }
+
+        public void InsertInOrder(int value)
+
+        {
+
+            Node newNode = new Node(value);
+            
+
+            if (isEmpty())
+            {
+                Head = newNode;
+                Last = Head;
+                UpdateMiddle();
+
+            }
+
+            else if (newNode.Data >= Last.Data)
+            {
+                InsertLast(newNode.Data);
+                UpdateMiddle();
+            }
+
+            // Caso 3: Insertar en el lugar correcto dentro de la lista
+            else
+            {
+                Node index = Head;
+
+                // Encuentra la posición adecuada
+                while (index != null && value >= index.Data)
+                {
+                    index = index.Next;
+                }
+
+                // Si el nuevo nodo debe ir al principio
+                if (index == Head)
+                {
+                    newNode.Next = Head;
+                    Head.Previous = newNode;
+                    Head = newNode;
+                    UpdateMiddle();
+                }
+                // Si el nuevo nodo debe ir en cualquier otro lugar (antes de "index")
+                else
+                {
+                    newNode.Previous = index.Previous;
+                    newNode.Next = index;
+
+                    if (index.Previous != null)
+                    {
+                        index.Previous.Next = newNode;
+                    }
+
+                    index.Previous = newNode;
+                    UpdateMiddle();
+                }
+            }
+
+            Count++;
+            UpdateMiddle();
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Problemas/Program.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problemas
+{
+    public class Program
+    {
+        public enum SortDirection
+        {
+            Ascending,
+            Descending
+        }
+
+        public DoublyLinkedList MergeSorted(DoublyLinkedList A, DoublyLinkedList B, SortDirection Direction)
+        {
+            Console.WriteLine("Merging:");
+            A.PrintList();
+            B.PrintList();
+
+            if (A == null || B == null)
+            {
+                throw new ArgumentNullException("Both A and B must be of type DoublyLinkedList");
+            }
+
+            if (A.Head == null) // Si A está vacía, copiar todos los nodos de B a A.
+            {
+                A.Head = B.Head;
+                A.Last = B.Last;
+                A.Count = B.Count;
+
+                if (Direction != SortDirection.Ascending)
+                {
+                    InvertList(A);
+                }
+
+                A.PrintList();
+                A.UpdateMiddle();
+                return A;
+            }
+
+            // Si la lista B está vacía, simplemente devuelve A (invertida si es necesario)
+            if (B.Head == null)
+            {
+                if (Direction != SortDirection.Ascending)
+                {
+                    InvertList(A);
+                }
+                A.PrintList();
+                A.UpdateMiddle();
+                return A;
+            }
+
+            // Fusión de las listas A y B
+            Node currentA = A.Head;
+            Node currentB = B.Head;
+
+            while (currentB != null)
+            {
+                if (currentA == null) // Si A se termina, inserta todo B en A
+                {
+                    A.Last.Next = currentB;
+                    currentB.Previous = A.Last;
+                    A.Last = B.Last;
+                    A.Count += B.Count;
+                    break;
+                }
+                else if (currentB.Data < currentA.Data)
+                {
+                    // Insertar el nodo de B antes del nodo actual de A
+                    Node nextB = currentB.Next;
+                    if (currentA.Previous == null) // Insertar al principio de A
+                    {
+                        currentB.Next = A.Head;
+                        A.Head.Previous = currentB;
+                        A.Head = currentB;
+                    }
+                    else
+                    {
+                        currentB.Previous = currentA.Previous;
+                        currentA.Previous.Next = currentB;
+                        currentB.Next = currentA;
+                        currentA.Previous = currentB;
+                    }
+                    A.Count++;
+                    currentB = nextB;
+                }
+                else
+                {
+                    currentA = currentA.Next; // Continuar recorriendo A
+                }
+            }
+
+            // Si es necesario invertir la lista final
+            if (Direction != SortDirection.Ascending)
+            {
+                InvertList(A);
+                A.UpdateMiddle();
+            }
+
+            // Actualizar el nodo del medio después de la fusión
+            A.UpdateMiddle();
+            A.PrintList();
+            return A;
+        }
+
+
+        public DoublyLinkedList InvertList(DoublyLinkedList list)
+        {
+
+            if (list == null)
+            {
+                throw new ArgumentNullException("list must be of type DoublyLinkedList");
+            }
+
+
+            Node current = list.Head;
+            Node temp = null;
+
+            // Intercambiamos los punteros Next y Previous de cada nodo
+            while (current != null)
+            {
+                temp = current.Previous;
+                current.Previous = current.Next;
+                current.Next = temp;
+                current = current.Previous; // Nos movemos al siguiente nodo que ahora es Previous
+            }
+
+            // Ajustamos los punteros Head y Last
+            if (temp != null)
+            {
+                list.Last = list.Head;
+                list.Head = temp.Previous;
+            }
+
+            return list;
+        }
+
+
+        public int GetMiddle(DoublyLinkedList list)
+        {
+            if (list == null)
+            {
+                throw new ArgumentNullException("List is null");
+            }
+
+            else if (list.Head == null)
+                    {
+                        throw new ArgumentNullException("List is empty");
+                    }
+
+
+                    else
+                    { return list.Middle.Data; }
+        }
+
+
+
+
+        public static void Main(string[] args)
+        {
+            // Crear listas A y B
+            DoublyLinkedList A = new DoublyLinkedList();
+            DoublyLinkedList B = new DoublyLinkedList();
+
+            Program p = new Program();
+
+            // Insertar valores en la lista A (orden ascendente)
+            A.InsertLast(10);
+            A.InsertLast(60);
+
+            Console.WriteLine("El nodo del medio de la lista A es: " + p.GetMiddle(A));
+
+
+            // Insertar valores en la lista B (orden ascendente)
+            B.InsertLast(9);
+            B.InsertLast(40);
+            B.InsertLast(50);
+            B.InsertInOrder(35);
+            B.InsertInOrder(25);
+
+            Console.WriteLine("El nodo del medio de la lista B es: " + p.GetMiddle(B));
+
+
+            // Mostrar listas antes de la fusión
+            Console.WriteLine("Antes de la fusión:");
+            Console.Write("Lista A: ");
+            A.PrintList();
+            Console.Write("Lista B: ");
+            B.PrintList();
+
+            // Fusionar listas
+            
+            p.MergeSorted(A, B, Program.SortDirection.Descending);
+
+            // Mostrar listas después de la fusión
+            Console.WriteLine("Después de la fusión:");
+            A.PrintList();
+
+            Console.WriteLine("El nodo del medio de la lista A es: " + p.GetMiddle(A));
+        }
+
+    }
+
+
+}
+
+    
